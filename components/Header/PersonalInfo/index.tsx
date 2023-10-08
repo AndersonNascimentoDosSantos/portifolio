cd4d9f71e08fead39cbcdf60e1b227a13d35a01b
Anderson Nascimento dos Santos <tec.anderson.ns@gmail.com>
2023-10-08 07:50:50 -0300

    feat: Main menu Personal information

---
+++ components/Header/PersonalInfo/index.tsx
@@ -0,0 +1,8 @@
+export default function PersonalInfo() {
+  return (
+    <div className="flex flex-col gap-1">
+      <span>Anderson Nascimento dos Santos</span>
+      <span>FullStack Developer</span>
+    </div>
+  );
+}
