5a1e7e999e0ffe158be208ab23fcd312deeccc30
Anderson Nascimento dos Santos <tec.anderson.ns@gmail.com>
2023-10-09 13:36:30 -0300

    refactor: slider placed

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,36 @@
+"use client";
+import { ProjectCard } from "@/components/Commoms/ProjectCard";
 import { ContactForm } from "@/components/ContactForm";
 
-export default function Home() {
+import { SliderSwipe } from "@/components/Slider";
+import { SHEET_STACKS } from "@/utils/constants";
+import { ThemeProvider } from "@material-tailwind/react";
+const Home = () => {
   return (
-    <main className="flex min-h-screen flex-col  justify-between p-8 w-[full]">
-      <div className="grid grid-cols-2 grid-rows-1 h-[full] gap-5">
-        <section className="flex border h-[full] flex-col flex-grow">
-          <ContactForm />
-        </section>
-        <section>stack slide</section>
-      </div>
-    </main>
+    <ThemeProvider>
+      <main className="flex min-h-screen flex-col  justify-between p-8 w-[full]">
+        <div className="grid grid-cols-3 grid-rows-1 h-[full] gap-5">
+          <section className="flex  h-[full] flex-col flex-grow flex-[2] col-span-2">
+            <ContactForm />
+          </section>
+          <section className="flex flex-col flex-[1] col-span-1">
+            stack slide
+            <SliderSwipe
+              loop
+              autoplay
+              pagination
+              items={SHEET_STACKS.slides || []}
+              classes={{ slideItem: "!w-full" }}
+              reactKeyProp="id"
+              renderSlideItem={(item) => {
+                return <ProjectCard index={item.id} />;
+              }}
+            />
+          </section>
+        </div>
+      </main>
+    </ThemeProvider>
   );
-}
+};
+
+export default Home;
