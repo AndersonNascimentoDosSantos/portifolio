1d01dff6b9e03e7fcf1377dbc7e7fa7c75cfaf65
anderson nascimento dos santos <tec.anderson.ns@gmail.com>
2023-12-15 13:28:15 -0300

    fix:build error

--- components/Slider/index.tsx
+++ components/Slider/index.tsx
@@ -150,7 +150,7 @@ export const SliderSwipe = <DataType extends unknown>({
 
 export function Slider() {
   return (
-    <Carousel className="rounded-xl" autoplay loop>
+    <Carousel className="rounded-xl" autoplay loop placeholder={""}>
       <img
         src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
         alt="image 1"
